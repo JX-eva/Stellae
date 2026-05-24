e7316e78f8b9e1fe925a135d89de9db82cbdd8dc
JX <1721227870@qq.com>
2026-05-25 03:07:05 +0800

    feat(init):init

---
+++ stellae-backend/src/main/java/io/github/jxeva/stellae/StellaeApplication.java
@@ -0,0 +1,13 @@
+package io.github.jxeva.stellae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StellaeApplication.class, args);
+    }
+
+}
---
+++ stellae-backend/src/test/java/io/github/jxeva/stellae/StellaeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jxeva.stellae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StellaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
